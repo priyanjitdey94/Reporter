6056b4f8356a0ac06224252cf6b8bdf7cacd6bed
Priyanjit <priyanjit@fusioncharts.com>
2019-03-05 14:20:36 +0530

    created loginh page

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,36 @@ import React, { Component } from 'react';
 import Uploader from './components/uploader';
 import './App.css';
 import Header from './components/Header';
+import Login from './components/Login';
 
 class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      userInfo: this.isSessionPresent()
+    };
+  }
   render() {
+    let { userInfo } = this.state,
+      bodyVisual;
+
+    bodyVisual = userInfo ? <Uploader /> : <Login onUserAuth={this.onUserAuth} />;
     return (
       <div className="App">
         <Header />
-        <Uploader />
+        {bodyVisual}
       </div>
     );
+  }
+  
+  isSessionPresent = () => {
+    return !!(localStorage.getItem('jiraReporterUser'));
+  }
+
+  onUserAuth = () => {
+    this.setState({
+      userInfo: true
+    });
   }
 }
 
