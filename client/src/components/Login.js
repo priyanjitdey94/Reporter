6ca96f9c1f53fdbcad5252c87c3b3eff55e4edf0
Priyanjit <priyanjit@fusioncharts.com>
2019-03-05 17:44:46 +0530

    setup server

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import '../css/login.css';
+import axios from 'axios';
+// import JiraConnector from 'jira-connector';
 // import cryptojs from 'crypto-js';
 
 class Login extends Component {
@@ -22,10 +24,42 @@ class Login extends Component {
   authorizeLogin = () => {
     let username = document.getElementById('username').value,
       password = document.getElementById('password').value;
-
-    localStorage.setItem('jiraReporterUser', username);
-    localStorage.setItem('jiraReporterPassword', password);
-    this.props.onUserAuth && this.props.onUserAuth();
+    
+    // var jira = new JiraConnector( {
+    //   host: 'fusioncharts.jira.com',
+    //   basic_auth: {
+    //       username: 'SirUserOfName',
+    //       password: 'Password123'
+    //   }
+    // });
+    // console.log(jira.user.getUser({
+    //   username,
+    //   password
+    // }));
+    axios({
+      method: 'get',
+      url: 'https://fusioncharts.jira.com/rest/api/2/issue/createmeta',
+      auth: {
+        username,
+        password
+      },
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+        'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
+        'Access-Control-Max-Age': 86400,
+        'Content-Type': 'application/json',
+        Pragma: 'no-cache',
+        'Access-Control-Expose-Headers': 'Access-Token, Uid'
+      }
+    }).then(function (response) {
+      console.log(response);
+    }).catch(function (err) {
+      console.log(err);
+    });
+    // localStorage.setItem('jiraReporterUser', username);
+    // localStorage.setItem('jiraReporterPassword', password);
+    // this.props.onUserAuth && this.props.onUserAuth();
   }
 }
 
