858704c1afdefe0fb44d64e49875bfea0240b645
Priyanjit <priyanjit@fusioncharts.com>
2019-03-06 15:58:17 +0530

    modified modal

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -4,51 +4,51 @@ import '../css/modal.css';
 class Modal extends Component {
   render () {
     return (
-      <div className='modal-background' onClick={this.handler}>
+      <div className='modal-background'>
         <div className='modal-container'>
           <div className='modal-header'>
-            <button className='modal-header-button'>X</button>
+            <button className='modal-header-button'  onClick={this.handler} >X</button>
           </div>
           <div className='modal-body'>
             <div className='data-selector'>
-              <label>Project</label>
+              <label className='data-label'>Project</label>
               <input type='text'></input>
             </div>
             <div className='data-selector'>
-              <label>Issue type</label>
+              <label className='data-label'>Issue type</label>
               <input type='text'></input>
             </div>
             <div className='data-selector'>
-              <label>Summary</label>
+              <label className='data-label'>Summary</label>
               <input type='text'></input>
             </div>
             <div className='data-selector'>
-              <label>Priority</label>
+              <label className='data-label'>Priority</label>
               <input type='text'></input>
             </div>
             <div className='data-selector'>
-              <label>Affect Version</label>
+              <label className='data-label'>Affect Version</label>
               <input type='text'></input>
             </div>
             <div className='data-selector'>
-              <label>Assignee</label>
+              <label className='data-label'>Assignee</label>
               <input type='text'></input>
             </div>
             <div className='data-selector'>
-              <label>Reviewer</label>
+              <label className='data-label'>Reviewer</label>
               <input type='text'></input>
             </div>
             <div className='data-selector'>
-              <label>Reporter</label>
+              <label className='data-label'>Reporter</label>
               <input type='text'></input>
             </div>
             <div className='data-selector'>
-              <label>Description</label>
-              <input type='textarea'></input>
+              <label className='data-label'>Description</label>
+              <textarea className='data-textarea'></textarea>
             </div>
           </div>
           <div className='modal-footer'>
-            <button className='modal-footer-button'>Cancel</button>
+            <button className='modal-footer-button'  onClick={this.handler} >Cancel</button>
             <button className='modal-footer-button'>Apply</button>
           </div>
         </div>
