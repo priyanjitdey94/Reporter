6f8db65337945c9f040eac0460682544d4616bde
Ashish Mishra <ashish@fusioncharts.com>
2019-03-08 18:14:09 +0530

    style changes

--- client/src/components/SideBar.js
+++ client/src/components/SideBar.js
@@ -17,15 +17,15 @@ export default class SideBar extends Component {
     return (
       <div className="side-bar">
         <div className="project-selector">
-        <label>Select Project</label>
+        <label className='sidebar-label'>Select Project</label>
         <Select isSearchable={true} options={projectOptions} onChange={(e) => {this.handleProjectChange(e.value)}}/>
         </div>
         <div className={this.props.project ? 'key-selector' : 'hide'}>
-        <label>Select Key</label>
+        <label className='sidebar-label'>Select Key</label>
         <Select isSearchable={true} options={[{value: 'issuetype', label: 'Issue Type'}, {value: 'fixversion', label: 'Fix Version'}, {value: 'affectversion', label: 'Affect Version'}, {value: 'priority', label: 'Priority'}, {value: 'assignee', label: 'Assignee'}]} onChange={(e) => {this.handleKeyChange(e.value)}}/>
         </div>
         <div className={this.props.project ? 'value-selector' : 'hide'}>
-        <label>Select Value</label>
+        <label className='sidebar-label'>Select Value</label>
         <Select isSearchable={true} options={this.getValueOptions()} onChange={(e) => {this.handleValueChange(e.value)}}/>
         </div>
       </div>
